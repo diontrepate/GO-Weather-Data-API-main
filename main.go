e99e471106b035de97bda2b211e577a4ebb42fb6
DiontrePate <zdpate@tiu.edu>
2020-12-23 19:39:22 -0600

    added api call  to open weather api added env

--- main.go
+++ main.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load("configurations/openWeatherConfig.env")
 
 	// := used for declaration, it can also assign values
+	// This returns a default engine with a logger already attached
 	r := gin.Default()
 
 	// The * denotes the variable variable suffixed as a pointer
-	r.GET("/start", func(c *gin.Context) {
-		retWeather := Weather{Day: "Monday", Temp: 86}
+	r.GET("/weather-data", func(c *gin.Context) {
 
-		actual, err := json.Marshal(retWeather)
+		// The _ allows us to bypass the other return types
+		response, _ := http.Get("http://api.openweathermap.org/data/2.5/weather?q=Kenosha&appid=" + os.Getenv("API_KEY"))
+
+		responseData, err := ioutil.ReadAll(response.Body)
 
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		c.Data(http.StatusOK, gin.MIMEJSON, actual)
+		c.Data(http.StatusOK, gin.MIMEJSON, responseData)
 
 		fmt.Println("SUCCESS")
 	})
 
 	r.Run()
-}
-
-// Made capitolized to allow export ability
-type Weather struct {
-	Day  string
-	Temp int
 }
